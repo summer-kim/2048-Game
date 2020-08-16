cf34cb17ab078bcba9a268f89480f86b1fee0788
summer-kim <48466707+summer-kim@users.noreply.github.com>
2020-08-16 17:20:49 +0900

    2038 Game part2 (drag&move)

--- 2048.js
+++ 2048.js
@@ -31,3 +31,157 @@ function createNum() {
 
 init();
 createNum();
+
+var startDrag = false;
+var iAmDragging = false;
+var startPosition;
+
+var targetStart;
+var targetEnd;
+
+table.addEventListener("mousedown", function (e) {
+  startDrag = true;
+  startPosition = [e.clientX, e.clientY];
+  targetStart = e;
+});
+
+table.addEventListener("mousemove", function () {
+  if (startDrag) {
+    iAmDragging = true;
+  }
+});
+
+table.addEventListener("mouseup", function (e) {
+  var direction;
+  if (iAmDragging) {
+    lastPosition = [e.clientX, e.clientY];
+    differenceX = lastPosition[0] - startPosition[0];
+    differenceY = startPosition[1] - lastPosition[1];
+    if (differenceY > differenceX) {
+      if (differenceY > -differenceX) {
+        direction = "up";
+      } else {
+        direction = "left";
+      }
+    } else if (differenceY < differenceX) {
+      if (differenceY < -differenceX) {
+        direction = "down";
+      } else {
+        direction = "right";
+      }
+    }
+    targetEnd = e;
+    moveBlock(direction);
+  }
+  startDrag = false;
+  iAmDragging = false;
+});
+
+function moveBlock(direction) {
+  switch (direction) {
+    case "up":
+      var newData = [[], [], [], []];
+      data.forEach(function (rowData, i) {
+        rowData.forEach(function (colData, j) {
+          if (colData) {
+            newData[j].push(colData);
+          }
+        });
+      });
+      [1, 2, 3, 4].forEach(function (rowData, i) {
+        [1, 2, 3, 4].forEach(function (colData, j) {
+          data[i][j] = newData[j][i] || 0;
+        });
+      });
+      data.forEach(function (rowData, i) {
+        rowData.forEach(function (colData, j) {
+          if (colData) {
+            table.children[i].children[j].textContent = data[i][j];
+          } else {
+            table.children[i].children[j].textContent = "";
+          }
+        });
+      });
+      createNum();
+      console.log("up");
+      break;
+    case "left":
+      newData = [[], [], [], []];
+      data.forEach(function (rowData, i) {
+        rowData.forEach(function (colData, j) {
+          if (colData) {
+            newData[i].push(colData);
+          }
+        });
+      });
+      [1, 2, 3, 4].forEach(function (rowData, i) {
+        [1, 2, 3, 4].forEach(function (colData, j) {
+          data[i][j] = newData[i][j] || 0;
+        });
+      });
+      data.forEach(function (rowData, i) {
+        rowData.forEach(function (colData, j) {
+          if (colData) {
+            table.children[i].children[j].textContent = data[i][j];
+          } else {
+            table.children[i].children[j].textContent = "";
+          }
+        });
+      });
+      createNum();
+      console.log("left");
+      break;
+    case "down":
+      newData = [[], [], [], []];
+      data.forEach(function (rowData, i) {
+        rowData.forEach(function (colData, j) {
+          if (colData) {
+            newData[j].push(colData);
+          }
+        });
+      });
+      [1, 2, 3, 4].forEach(function (rowData, i) {
+        [1, 2, 3, 4].forEach(function (colData, j) {
+          data[3 - i][j] = newData[j][i] || 0;
+        });
+      });
+      data.forEach(function (rowData, i) {
+        rowData.forEach(function (colData, j) {
+          if (colData) {
+            table.children[i].children[j].textContent = data[i][j];
+          } else {
+            table.children[i].children[j].textContent = "";
+          }
+        });
+      });
+      createNum();
+      console.log("down");
+      break;
+    case "right":
+      newData = [[], [], [], []];
+      data.forEach(function (rowData, i) {
+        rowData.forEach(function (colData, j) {
+          if (colData) {
+            newData[i].push(colData);
+          }
+        });
+      });
+      [1, 2, 3, 4].forEach(function (rowData, i) {
+        [1, 2, 3, 4].forEach(function (colData, j) {
+          data[i][3 - j] = newData[i][j] || 0;
+        });
+      });
+      data.forEach(function (rowData, i) {
+        rowData.forEach(function (colData, j) {
+          if (colData) {
+            table.children[i].children[j].textContent = data[i][j];
+          } else {
+            table.children[i].children[j].textContent = "";
+          }
+        });
+      });
+      createNum();
+      console.log("right");
+      break;
+  }
+}
