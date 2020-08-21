37a2f064e9283f1b05728ba288350f52b911f280
summer-kim <48466707+summer-kim@users.noreply.github.com>
2020-08-22 01:58:09 +0900

    Update 2048.js

--- 2048.js
+++ 2048.js
@@ -8,7 +8,33 @@ function init() {
     var div = document.createElement("div");
     div.innerHTML = 0;
     grid.appendChild(div);
+    data.push(div);
+  }
+  createRandomly();
+  createRandomly();
+}
+
+function createRandomly() {
+  num = Math.floor(Math.random() * data.length);
+  if (data[num].innerHTML === "0") {
+    data[num].innerHTML = 2;
+  } else {
+    createRandomly();
+  }
+}
+
+function ifMoveRight() {
+  for (i = 0; i < data.length; i++) {
+    if (i % width === 0) {
+      t1 = data[i].innerHTML;
+      t2 = data[i + 1].innerHTML;
+      t3 = data[i + 2].innerHTML;
+      t4 = data[i + 3].innerHTML;
+      temp = [t1, t2, t3, t4];
+      console.log(temp);
+    }
   }
 }
 
 init();
+ifMoveRight();
